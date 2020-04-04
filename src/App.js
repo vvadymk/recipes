7f398e1661e5331024f927049bf575f8a979b9ba
elec-trick <kochmar.vadym@gmail.com>
2020-04-04 21:45:32 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeScreen from './screens/HomeScreen'
+import AddScreen from './screens/AddScreen'
+import EditScreen from './screens/EditScreen'
+import RecipeScreen from "./screens/RecipeScreen";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <div>
+                    <NavBar />
+                    <Switch>
+                        <Route path="/add" component={AddScreen} />
+                        <Route path="/edit/:id" component={EditScreen} />
+                        <Route path="/recipes/:id" component={RecipeScreen} />
+                        <Route path="/" component={HomeScreen} />
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/screens/RecipeScreen.js
@@ -0,0 +1,100 @@
+import React, {useEffect, useState} from 'react';
+import {
+    Container,
+    Row,
+    Col,
+    Button, Card
+} from 'react-bootstrap';
+import services from '../services';
+import faker from "faker";
+import {Link} from "react-router-dom";
+
+const RecipeScreen = ({match: { params }}) => {
+    const id = params.id;
+    const [name, setName] = useState('');
+    const [shortDesc, setShortDesc] = useState('');
+    const [longDesc, setLongDesc] = useState('');
+    const [createDate, setCreateDate]=useState('');
+
+    const loadChanges = async () => {
+        try{
+            const resp = await services.getRecipe(id);
+            console.log(resp);
+            const recipe = resp.data;
+            setLongDesc(recipe.longDesc);
+            setName(recipe.name);
+            setShortDesc(recipe.shortDesc);
+            setCreateDate(recipe.createDate);
+        }catch (error) {
+            console.log(error);
+            alert('Failed changes');
+        }
+    };
+
+    useEffect(()=>{
+        loadChanges()
+    }, []);
+
+    const deleteRecipe = async () => {
+        try{
+            await services.deleteRecipe(id);
+            window.location.reload();
+        }catch (error) {
+            console.log(error);
+            alert("Delete failed");
+        }
+    };
+
+    return (
+        <Container>
+            <Row style={{margin: "10px"}}>
+                <Col lg={4}>
+                    <Card.Img variant="top" src={faker.image.food()} />
+                </Col>
+                <Col lg={5}>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        {createDate}
+                    </Card.Text>
+                    <Card.Text>
+                        {shortDesc}
+                    </Card.Text>
+                    <Link to={`/edit/${id}`}>
+                        <Button
+                            variant="primary"
+                            style={{marginRight: "10px"}}
+                        >
+                            Edit
+                        </Button>
+                    </Link>
+                    <Link to={`/`}>
+                        <Button
+                            variant="danger"
+                            style={{marginRight: "10px"}}
+                            onClick={deleteRecipe}
+                        >
+                            Delete
+                        </Button>
+                    </Link>
+                    <Link to={`/`}>
+                        <Button
+                            variant="success"
+                        >
+                            Back
+                        </Button>
+                    </Link>
+                </Col>
+            </Row>
+            <Row className="justify-content-md-center" >
+                <Col lg={0}>
+                    <h2>Full recipe:</h2>
+                    <Card.Text>
+                        {longDesc}
+                    </Card.Text>
+                </Col>
+            </Row>
+        </Container>
+    )
+};
+
+export default RecipeScreen;
